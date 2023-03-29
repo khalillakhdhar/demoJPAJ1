c06e5da70af2e98b5cbc13dc46ce521c11a89e54
khalil <khalillakhdharatc@gmail.com>
2023-03-29 10:25:27 +0100

    Initial commit

---
+++ src/main/java/com/jpa/DemoJpaj1Application.java
@@ -0,0 +1,13 @@
+package com.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaj1Application.class, args);
+	}
+
+}
